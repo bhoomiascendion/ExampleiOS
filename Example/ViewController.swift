778d778baa990ef68c26d29d98184c96ee40acff
Bhoomi Kathiriya <bhoomi.kathiria@ascendion.com>
2024-03-28 15:21:04 +0530

    Added button action

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var greetingLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func helloButton_Clicked(_ sender: Any)   {
+        greetingLabel.text =  "Hello \(nameTextField.text ?? "")!"
+    }
 
 }
 
